8c49440c0bbd476a83e71029a6b37a41698e6481
Demond Lash <your-github-demondlash@gmail.com>
2026-08-31 04:33:45 +0000

    Draft HQ: Batch B complete

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ from model_calibration import model_health
 from adaptive_draft_reconciliation import apply_calibrated_reconciliation
 from draft_outcome_tracker import log_and_resolve
 from draft_accuracy_routes import create_draft_accuracy_blueprint
+from draft_outcome_health import create_outcome_health_blueprint
 from sleeper_opponent_forecast import reconcile_opponent_forecast
 from dynamic_need_model import calculate_dynamic_need
 from balanced_recommendation_score import calculate_balanced_score
@@ -3444,6 +3445,7 @@ app.register_blueprint(create_sleeper_hub_blueprint(get_db_connection))
 app.register_blueprint(create_sleeper_intelligence_blueprint(get_db_connection))
 
 app.register_blueprint(create_draft_accuracy_blueprint(get_db_connection))
+app.register_blueprint(create_outcome_health_blueprint(get_db_connection))
 
 app.register_blueprint(create_draft_health_blueprint(get_db_connection, SLEEPER_LEAGUE_ID, 2026, build_sleeper_draft_signals, model_health))
 
